ce5053d553e313e833b4dfc64b3d84ba3e8e946b
Jordi Masip <masipcat@users.noreply.github.com>
2019-10-15 13:51:09 +0000

    Fix README.md in setup.py (#6)

--- setup.py
+++ setup.py
@@ -3,34 +3,24 @@ from setuptools import setup
 
 
 try:
-    README = open('README.rst').read()
+    README = open("README.md").read()
 except IOError:
     README = None
 
 setup(
-    name='guillotina_evolution',
-    version="1.0.0",
-    description=' ',
+    name="guillotina_evolution",
+    version="1.0.1",
+    description=" ",
     long_description=README,
-    install_requires=[
-        'guillotina>=5.0,<6.0',
-    ],
-    author='Jordi Masip',
-    author_email='jmasip@vinissimus.com',
-    url='https://github.com/vinissimus/guillotina_evolution',
-    packages=find_packages(exclude=['demo']),
+    long_description_content_type="text/markdown",
+    install_requires=["guillotina>=5.0,<6.0"],
+    author="Jordi Masip",
+    author_email="jmasip@vinissimus.com",
+    url="https://github.com/vinissimus/guillotina_evolution",
+    packages=find_packages(exclude=["demo"]),
     include_package_data=True,
-    tests_require=[
-        'pytest',
-    ],
-    extras_require={
-        'test': [
-            'pytest',
-            'pytest-asyncio',
-            'pytest-cov',
-            'coverage',
-        ]
-    },
+    tests_require=["pytest"],
+    extras_require={"test": ["pytest", "pytest-asyncio", "pytest-cov", "coverage"]},
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
@@ -39,6 +29,5 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
-    entry_points={
-    }
+    entry_points={},
 )
