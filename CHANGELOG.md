607d17fb36899396d3265491417e101d3baeee4e
Jordi Masip <masipcat@users.noreply.github.com>
2019-12-09 11:36:51 +0000

    Added support for Guillotina 6 (#7)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+1.1.0
+-----
+ - Added support for Guillotina 6
+   [masipcat]
+
 1.0.0
 -----
  - Upgraded codebase for Guillotina 5
--- setup.py
+++ setup.py
@@ -9,11 +9,11 @@ except IOError:
 
 setup(
     name="guillotina_evolution",
-    version="1.0.1",
+    version="1.1.0",
     description=" ",
     long_description=README,
     long_description_content_type="text/markdown",
-    install_requires=["guillotina>=5.0,<6.0"],
+    install_requires=["guillotina>=5.0,<7.0"],
     author="Jordi Masip",
     author_email="jmasip@vinissimus.com",
     url="https://github.com/vinissimus/guillotina_evolution",
@@ -22,12 +22,12 @@ setup(
     tests_require=["pytest"],
     extras_require={"test": ["pytest", "pytest-asyncio", "pytest-cov", "coverage"]},
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Natural Language :: English",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     entry_points={},
 )
